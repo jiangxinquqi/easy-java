b69d5f8f97b3837f25aece332887ce9ae45e33d5
xiaojianjun <xiaoxiao65535@163.com>
2021-07-17 21:24:21 +0800

    solr测试demo

--- easy-springboot/easy-springboot-solrJ/src/test/java/com/xiao/solr/Default01CollectionTest.java
+++ easy-springboot/easy-springboot-solrJ/src/test/java/com/xiao/solr/Default01CollectionTest.java
@@ -25,7 +25,8 @@ public class Default01CollectionTest {
 
     private CloudSolrClient solrClient;
 
-    private static final String name = "我爱这土地 【作者】bai艾青【朝代】du现代 假如我是一只鸟,我也应该用嘶哑的喉zhi咙歌唱: 这被dao暴风雨所打击着的土地,这永远汹涌着我们的悲愤的河流, 这无止息地吹刮着的激怒的风";
+    private static final String DEFAULT_01_COLLECTION = "default_01";
+    private static final String NAME = "我爱这土地 【作者】bai艾青【朝代】du现代 假如我是一只鸟,我也应该用嘶哑的喉zhi咙歌唱: 这被dao暴风雨所打击着的土地,这永远汹涌着我们的悲愤的河流, 这无止息地吹刮着的激怒的风";
 
     @Before
     public void init() {
@@ -34,7 +35,7 @@ public class Default01CollectionTest {
         solrUrls.add("http://solr02.cloud.com:8983/solr");
         solrUrls.add("http://solr03.cloud.com:8983/solr");
         solrClient = new CloudSolrClient.Builder(solrUrls).build();
-        solrClient.setDefaultCollection("default_01");
+        solrClient.setDefaultCollection(DEFAULT_01_COLLECTION);
         solrClient.connect();
     }
 
@@ -42,7 +43,7 @@ public class Default01CollectionTest {
     public void testAddDocument() throws IOException, SolrServerException {
         SolrInputDocument document = new SolrInputDocument();
         document.addField("id", "01_" + System.currentTimeMillis());
-        document.addField("name_cn", name);
+        document.addField("name_cn", NAME);
         solrClient.add(document);
         solrClient.commit();
     }
@@ -51,7 +52,7 @@ public class Default01CollectionTest {
     public void testAddBean() throws IOException, SolrServerException {
         Document document = new Document();
         document.setId("01_Bean_" + System.currentTimeMillis());
-        document.setName_cn(name);
+        document.setName_cn(NAME);
         solrClient.addBean(document);
         solrClient.commit();
     }
