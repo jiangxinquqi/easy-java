30219b1facf735eda8f0d60344fb745f31c20401
xiaocen <xiaocen65535@163.com>
2023-05-16 11:10:33 +0800

    一些demo

--- easy-springboot/easy-spring-boot-shallwe/src/main/java/com/xiao/shallwe/controller/MonitorController.java
+++ easy-springboot/easy-spring-boot-shallwe/src/main/java/com/xiao/shallwe/controller/MonitorController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 public class MonitorController {
 
     @GetMapping("/test")
-    public String test() {
-        return "hello world";
+    public HashMap<String, Object> test() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("message", "success");
+        map.put("data", "test");
+        return map;
     }
 
     @GetMapping("/monitor/{appName}/{option}")
@@ -19,7 +24,7 @@ public class MonitorController {
                           @PathVariable String option) throws IOException {
         String commond = "sh /opt/microservice/service/" + appName + "/bin/boot.sh " + option;
         Runtime.getRuntime().exec(commond);
-        return "sucess";
+        return "success";
     }
 
 }
